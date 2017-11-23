4f8c0a7d06e9a92d3a21c8aabc72b7760fdc28cd
davidwfranco <davidwfranco@gmail.com>
2017-11-23 18:16:31 -0200

    Added the basic structure of the game, some dummy blocks on the top, the ball and the bottom bar; created the system of the bar tracking the mouse and the boundaries around the screen.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	public static GameController instance;
+	public Camera cam;
+    public Vector2 upperCorner;
+    public Vector2 targeCamtWidth;
+    public float maxWidth;
+    public float maxHeigth;
+    public bool gameOver;
+    public GameObject gameOverText;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        upperCorner = new Vector2(Screen.width, Screen.height);
+        targeCamtWidth = GameController.instance.cam.ScreenToWorldPoint(upperCorner);
+        maxWidth = targeCamtWidth.x;
+        maxHeigth = targeCamtWidth.y;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Restart the Game
+		if (gameOver && Input.anyKey) 
+		{
+			//Make unity reload the scene currenctly active
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+    public void Endgame() 
+	{
+        gameOver = true;
+        gameOverText.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rdb2d;
+	private Vector2 rawPosition;
+	private Vector2 targetPotision;
+	private float targetWidth;
+	private float playerWidth;
+	private float maxW;
+	private GameController gControl;
+
+	// Use this for initialization
+	void Start () {
+		rdb2d = GetComponent<Rigidbody2D>();
+		playerWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+		maxW = GameController.instance.maxWidth;
+		gControl = GameController.instance;
+	}
+	
+	// Update is called once per physics timestamp
+	void FixedUpdate () {
+		if (!GameController.instance.gameOver) {
+			rawPosition = GameController.instance.cam.ScreenToWorldPoint (Input.mousePosition);
+			targetPotision = new Vector2 (rawPosition.x, rdb2d.position.y);
+
+			// targetWidth = Mathf.Clamp (targetPotision.x, -maxW + (playerWidth/2), 
+			// 							maxW - (playerWidth/2));
+			// targetPotision = new Vector2 (targetWidth, targetPotision.y);
+
+			rdb2d.MovePosition (targetPotision);
+		} else {
+			rdb2d.MovePosition (new Vector2 (0.0f, rdb2d.position.y));
+		}
+	}
+}
