0392be6f99c1e6c92a20d46e87a97a6819f6f540
davidwfranco <davidwfranco@gmail.com>
2017-11-24 14:17:40 -0200

    Created the bouncy material and put it in the ball object

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	private Rigidbody2D rdb2d;
+	public float vSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rdb2d = GetComponent<Rigidbody2D>();
+
+		rdb2d.velocity = new Vector2(0, vSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,29 +8,36 @@ public class PlayerController : MonoBehaviour {
 	private Vector2 rawPosition;
 	private Vector2 targetPotision;
 	private float targetWidth;
-	private float playerWidth;
-	private float maxW;
 	private GameController gControl;
 
 	// Use this for initialization
 	void Start () {
 		rdb2d = GetComponent<Rigidbody2D>();
-		playerWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-		maxW = GameController.instance.maxWidth;
 		gControl = GameController.instance;
 	}
 	
 	// Update is called once per physics timestamp
 	void FixedUpdate () {
-		if (!GameController.instance.gameOver) {
-			rawPosition = GameController.instance.cam.ScreenToWorldPoint (Input.mousePosition);
-			targetPotision = new Vector2 (rawPosition.x, rdb2d.position.y);
+		if (!gControl.gameOver) {
+			//keyboard control
+			if (Input.GetKey("left")) 
+			{
+				rdb2d.velocity = new Vector2(-5, 0);
+			}
+			else if (Input.GetKey("right")) 
+			{
+				rdb2d.velocity = new Vector2(5, 0);				
+			}
+			else 
+			{
+				rdb2d.velocity = Vector2.zero;
+			}
 
-			// targetWidth = Mathf.Clamp (targetPotision.x, -maxW + (playerWidth/2), 
-			// 							maxW - (playerWidth/2));
-			// targetPotision = new Vector2 (targetWidth, targetPotision.y);
+			//mouse pointer control
+			// rawPosition = gControl.cam.ScreenToWorldPoint (Input.mousePosition);
+			// targetPotision = new Vector2 (rawPosition.x, rdb2d.position.y);
 
-			rdb2d.MovePosition (targetPotision);
+			// rdb2d.MovePosition (targetPotision);
 		} else {
 			rdb2d.MovePosition (new Vector2 (0.0f, rdb2d.position.y));
 		}
