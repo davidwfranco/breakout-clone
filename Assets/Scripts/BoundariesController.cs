1a6534435e25e07d31cf06f765ddec6bda9b7266
davidwfranco <davidwfranco@gmail.com>
2018-02-06 01:35:17 -0200

    Houston we have a MainMenu!! / Work the hell off to make the screen borders "shake" when the ball hit / Change the colors and screen size to give a nicer feeling and juiceness

---
+++ Assets/Scripts/BoundariesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundariesController : MonoBehaviour {
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.collider.CompareTag("Ball"))
+		{
+			GetComponent<Animation>().Play();		
+		}
+	}
+}
